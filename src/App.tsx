7681ed87f4b4c56bb0657f30f37ce6644eac4b02
Gherman Chernov <gherman.chernov@gmail.com>
2026-03-10 18:14:16 +0300

    refactor for other tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import { Suspense } from 'react'
+import { Suspense, useState } from 'react'
 import './App.css'
 import React from 'react'
-import Login from './components/Login'
+import { Login } from './components/Login'
+
+type TASK_TYPE = 'LOGIN' | 'FETCH' | 'LOCAL_STORAGE' | 'HOVER' | 'VIEWPORT_SIZE' | 'WINDOW_SCROLL' | 'TOGGLE_PLUS'
+const CURRENT_TASK: TASK_TYPE = 'LOGIN'
 
 class ErrorBoundary extends React.Component<{ children: React.ReactNode }> {
   state = { hasError: false, error: '' }
@@ -24,14 +27,35 @@ class ErrorBoundary extends React.Component<{ children: React.ReactNode }> {
 }
 
 function App() {
-
+  const [currentTask, setCurrentTask] = useState(CURRENT_TASK)
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <ErrorBoundary>
-        <Login />
+        <div style={{marginBottom: '10px'}}>
+          <p>Select task type</p>
+          <select value={currentTask} onChange={(e) => {setCurrentTask(e.target.value as TASK_TYPE)}}>
+            <option value="LOGIN">Login (hook task 1)</option>
+            <option value="FETCH">Fetch (user hook task 1)</option>
+            <option value="LOCAL_STORAGE">Local Storage (user hook task 2)</option>
+            <option value="HOVER">Hover (user hook task 3)</option>
+            <option value="VIEWPORT_SIZE">Viewport size (user hook extra task 1)</option>
+            <option value="WINDOW_SCROLL">Window scroll (user hook extra task 2)</option>
+            <option value="TOGGLE_PLUS">Toggle plus (user hook extra task 2)</option>
+          </select>
+        </div>
+        <TaskSelector currentTask={currentTask} />
       </ErrorBoundary>
     </Suspense>
   )
+}
+
+function TaskSelector({ currentTask }: { currentTask: TASK_TYPE  }) {
+  switch (currentTask) {
+    case 'LOGIN':
+      return <Login />
+    default:
+      return null
+  }
 }
 
 export default App
